76e5a9f27d8ea6fda3d392640cb83e0235b9f5b9
arishem <arishem51@gmail.com>
2025-11-08 16:06:53 +0700

    feat: tcp connect & listen

--- Q1/Client/MainWindow.xaml.cs
+++ Q1/Client/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
+﻿using System.Net.Sockets;
+using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Client
 {
@@ -16,9 +9,32 @@ namespace Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string ServerHost = "localhost";
+        private const int ServerPort = 5000;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var client = new TcpClient())
+                {
+                    await client.ConnectAsync(ServerHost, ServerPort);
+                    using (var stream = client.GetStream())
+                    {
+                        byte[] data = Encoding.UTF8.GetBytes("Hello World");
+                        await stream.WriteAsync(data, 0, data.Length);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error connecting to server: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
--- Q1/Server/Program.cs
+++ Q1/Server/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const int port = 5000;
+
+var listener = new TcpListener(IPAddress.Any, port);
+listener.Start();
+
+Console.WriteLine($"Server listening on port {port}...");
+
+while (true)
+{
+    var client = await listener.AcceptTcpClientAsync();
+    Console.WriteLine("Client connected.");
+
+    _ = Task.Run(async () =>
+    {
+        try
+        {
+            using (client)
+            using (var stream = client.GetStream())
+            {
+                var buffer = new byte[1024];
+                var bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                
+                if (bytesRead > 0)
+                {
+                    var message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("Hello World");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error handling client: {ex.Message}");
+        }
+    });
+}
